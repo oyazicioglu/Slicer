8f5f90b33c6dd57e614602aea658bdce109cbef3
Ömer YAZICIOĞLU <oyazicioglu@hotmail.com>
2023-12-29 03:50:33 +0300

    Some updtes

--- __init__.py
+++ __init__.py
@@ -11,31 +11,61 @@ bl_info = {
     
 import bpy
 
-class LayoutDemoPanel(bpy.types.Panel):
+class DimensionSettings(bpy.types.PropertyGroup):
+    width: bpy.props.FloatProperty(
+        name='Width',
+        default=2200.0 
+    )
+
+    length: bpy.props.FloatProperty(
+        name='Length',
+        default=1700.0 
+    )
+
+    thickness: bpy.props.FloatProperty(
+        name='Thickness',
+        default=1.8 
+    )
+
+
+class VIEW3D_PT_slicer(bpy.types.Panel):
     """Creates a Panel in the scene context of the properties editor"""
-    bl_label = "Slicer"
-    bl_idname = "OBJECT_PT_layout"
     bl_space_type = 'PROPERTIES'
-    bl_region_type = 'WINDOW'
-    bl_context = "object"
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+    bl_category = "Slicer"
+    bl_label = "Slicer"
     
+    my_float = bpy.props.FloatProperty(name="Some Floating Point", default=0.0)
+
     @classmethod
     def poll(cls, context):
         return context.mode == 'OBJECT' and len(context.selected_objects) > 0 and context.active_object.type == "MESH"
 
     def draw(self, context):
         layout = self.layout
+        dimensionProps = context.scene.dimensions
+        
+        col = layout.column(align=True, heading="Dimensions")
+        col.prop(dimensionProps, "width")
+        col.prop(dimensionProps, "length")
+        col.prop(dimensionProps, "thickness")
+    
+    
+classes = (
+    DimensionSettings,
+    VIEW3D_PT_slicer,
+)
 
-        row = layout.row()
-    
-def execute(self, context):
-    return {'FINISHED'}
-    
 def register():
-    bpy.utils.register_class(LayoutDemoPanel)
+    for cls in classes:
+        bpy.utils.register_class(cls)
+    bpy.types.Scene.dimensions = bpy.props.PointerProperty(type=DimensionSettings)
 
 def unregister():
-    bpy.utils.unregister_class(LayoutDemoPanel)
+    del bpy.types.Scene.dimensions
+    for cls in reversed(classes):
+        bpy.utils.unregister_class(cls)
 
 if __name__ == "__main__":
     register()
